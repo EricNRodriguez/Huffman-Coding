e53be01e9e389143b15caf008eb0f5713f227348
Eric Rodriguez <rodriguez.eric.n@gmail.com>
2019-05-29 13:08:55 +1000

    upd

--- bitArray/bit_array.go
+++ bitArray/bit_array.go
@@ -7,11 +7,11 @@ import (
 type IntegerSet []uint64
 
 func (s *IntegerSet) Add(x uint64) {
-	cell, bit := math.Floor(float64(x)/64), x%64
-	for uint64(cell) >= uint64(len(*s)) {
+	cell, bit := uint64(math.Floor(float64(x)/64)), x%64
+	for cell >= uint64(len(*s)) {
 		*s = append(*s, 0)
 	}
-	(*s)[uint64(cell)] |= (1 << bit)
+	(*s)[cell] |= (1 << bit)
 }
 
 func (s *IntegerSet) Remove(x uint64) {
